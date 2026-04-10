06a46b66a9b81557e61fe2ff0dd56fe61a368351
yarednegatuassefa-coder <yarednegatuassefa@gmail.com>
2026-04-10 22:57:33 +0300

    Delete app/crm/patients/[id]/treatment-plan directory

--- app/crm/patients/[id]/treatment-plan/page.jsx
+++
@@ -1,5 +0,0 @@
-import TreatmentPlanBuilder from '@/components/TreatmentPlanBuilder';
-
-export default function TreatmentPlanPage({ params }) {
-  return <TreatmentPlanBuilder patientId={params.id} />;
-}
